7500f13d2eb59f3176e8711a7c5a7c28208d3b6d
Suman Dey <email.sumandey@gmail.com>
2018-11-08 21:17:18 +0530

    Add elastic search api

---
+++ promethium/src/main/java/com/promethium/client/ElasticClient.java
@@ -0,0 +1,53 @@
+package com.promethium.client;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import org.elasticsearch.client.Client;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.transport.client.PreBuiltTransportClient;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
+import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
+
+
+
+
+
+public class ElasticClient {
+
+	@Value("${elasticsearch.home:/usr/local/Cellar/elasticsearch/5.6.0}")
+    private String elasticsearchHome;
+
+    @Value("${elasticsearch.cluster.name:elasticsearch}")
+    private String clusterName;
+
+	
+	public static Client client;
+	
+	public Client client() {
+		
+		TransportClient client = null;
+        try {
+            final Settings elasticsearchSettings = Settings.builder()
+              .put("client.transport.sniff", true)
+              .put("cluster.name", clusterName)
+              .put("node.name", "aKGWEn3").build();
+            client = new PreBuiltTransportClient(elasticsearchSettings);
+            client.addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("52.205.181.28"), 9300));
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        return client;
+		
+	}
+	
+	@Bean
+    public ElasticsearchOperations elasticsearchTemplate() {
+        return new ElasticsearchTemplate(client());
+    }
+	
+}
---
+++ promethium/src/main/java/com/promethium/controller/ElasticSearch.java
@@ -0,0 +1,73 @@
+package com.promethium.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.promethium.client.ElasticClient;
+import com.promethium.models.ConnectionErrorResponse;
+import com.promethium.models.ResponseSuccess;
+
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.client.Client;
+import org.elasticsearch.common.xcontent.XContentType;
+
+@RestController
+public class ElasticSearch {
+	
+	public static Client client;
+	
+	
+	// Prepare the client
+	protected void getClient() {
+		
+		client = new ElasticClient().client();
+		
+	}
+	
+	
+	public void givenJsonString_whenJavaObject_thenIndexDocument() {
+	    String jsonObject = "{\"age\":10,\"dateOfBirth\":1471466076564,"
+	      +"\"fullName\":\"John Doe\"}";
+	    IndexResponse response = client.prepareIndex("people", "Doe")
+	      .setSource(jsonObject, XContentType.JSON).get();
+	       
+	    String id = response.getId();
+	    String index = response.getIndex();
+	    String type = response.getType();
+	    long version = response.getVersion();
+	    
+	    System.out.print(response.getResult());
+//	        
+//	    assertEquals(Result.CREATED, response.getResult());
+//	    assertEquals(0, version);
+//	    assertEquals("people", index);
+//	    assertEquals("Doe", type);
+	}
+
+	@SuppressWarnings("unchecked")
+	@RequestMapping(path = "/api/elasticseach", method={RequestMethod.POST})
+	@ResponseStatus(value=HttpStatus.OK)
+	public String requestElastic(@RequestParam("keyword") String _keyword) {
+		
+		
+		// first make the client
+		
+		getClient();
+		
+		givenJsonString_whenJavaObject_thenIndexDocument();
+		
+		return "deatails";
+	
+	}
+	
+}
